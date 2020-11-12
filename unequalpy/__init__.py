79d46602e13c2be3c3e37ff29ed505f6c01848ee
Lucia F. de la Bella <55983939+Lucia-Fonseca@users.noreply.github.com>
2020-11-12 11:44:19 +0000

    Update __init__.py

--- unequalpy/__init__.py
+++ unequalpy/__init__.py
@@ -2,4 +2,4 @@
 This package contains functions for the unequal-time matter power spectrum 
 and weak lensing analysis using DES Y1 and LSST Y10 redshift distributions.
 """
-__version__ = '0.1.5'
+__version__ = '0.1.3'
